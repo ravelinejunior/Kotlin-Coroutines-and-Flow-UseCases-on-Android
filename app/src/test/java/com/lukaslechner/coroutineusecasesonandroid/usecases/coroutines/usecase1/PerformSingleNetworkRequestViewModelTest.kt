4ee98043e8975df6886ab2fbf1bf6a948721dd25
ravelinejunior <junior.raveline@gmail.com>
2023-02-23 22:15:15 +0000

    Using the TestCoroutineDispatcher for our first unit test

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModelTest.kt
@@ -0,0 +1,66 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.*
+import org.junit.rules.TestRule
+
+@ExperimentalCoroutinesApi
+class PerformSingleNetworkRequestViewModelTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    private lateinit var dispatcher: TestCoroutineDispatcher
+
+    @Before
+    fun setUp() {
+        dispatcher = TestCoroutineDispatcher()
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        dispatcher.cleanupTestCoroutines()
+    }
+
+    private val receivedUiStates = mutableListOf<UiState>()
+
+    @Test
+    fun `performSingleNetworkRequest shouldReturnSuccessWhenRequestIsSuccessfully ReturnSuccess`() {
+
+        //Arrange
+        val fakeSuccessApi = FakeSuccessApi()
+        val viewModel = PerformSingleNetworkRequestViewModel(fakeSuccessApi)
+
+        observeUiState(viewModel)
+
+        //Act
+        viewModel.performSingleNetworkRequest()
+
+        //Assert
+        Assert.assertEquals(
+            listOf(
+                UiState.Loading,
+                UiState.Success(mockAndroidVersions)
+            ), receivedUiStates
+        )
+
+    }
+
+    private fun observeUiState(viewModel: PerformSingleNetworkRequestViewModel) {
+        viewModel.uiState().observeForever { uiState ->
+
+            if (uiState != null) {
+                receivedUiStates.add(uiState)
+            }
+
+        }
+    }
+}
