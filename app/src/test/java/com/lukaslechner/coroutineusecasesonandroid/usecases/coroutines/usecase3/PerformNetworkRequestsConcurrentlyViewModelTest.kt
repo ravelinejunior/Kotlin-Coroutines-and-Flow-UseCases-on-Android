6c3145619e975cb9a2e612723403f26547d9e184
ravelinejunior <junior.raveline@gmail.com>
2023-02-27 00:20:17 +0000

    Testing sequential and concurrent execution

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModelTest.kt
@@ -0,0 +1,105 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockVersionFeaturesAndroid10
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockVersionFeaturesOreo
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockVersionFeaturesPie
+import com.lukaslechner.coroutineusecasesonandroid.utils.MainCoroutineScopeRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.*
+
+@ExperimentalCoroutinesApi
+class PerformNetworkRequestsConcurrentlyViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainDispatcherRule = MainCoroutineScopeRule()
+
+    private val receivedStates = mutableListOf<UiState>()
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `performNetworkRequestsSequentially shouldLoadDataSequentially assertEquals`() =
+        mainDispatcherRule.runBlockingTest {
+
+            // Arrange
+            val responseDelayTime = 1000L
+            val mockApi = FakeSuccessMockApi(responseDelayTime)
+            val viewModel = PerformNetworkRequestsConcurrentlyViewModel(mockApi)
+            observeViewModel(viewModel)
+
+            // Act
+            viewModel.performNetworkRequestsSequentially()
+            val forwardedTime = testScheduler.apply { advanceTimeBy(3000); runCurrent() }
+
+            // Assert
+            Assert.assertEquals(
+                listOf(
+                    UiState.Loading, UiState.Success(
+                        listOf(
+                            mockVersionFeaturesOreo,
+                            mockVersionFeaturesPie,
+                            mockVersionFeaturesAndroid10
+                        )
+                    )
+                ), receivedStates
+            )
+
+            Assert.assertEquals(
+                3000, forwardedTime.currentTime
+            )
+        }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `performNetworkRequestsConcurrently shouldLoadDataConcurrently assertEquals`() =
+        mainDispatcherRule.runBlockingTest {
+
+            // Arrange
+            val responseDelay = 1000L
+            val mockApi = FakeSuccessMockApi(responseDelay)
+            val viewModel = PerformNetworkRequestsConcurrentlyViewModel(mockApi)
+            observeViewModel(viewModel)
+
+            // Act
+            viewModel.performNetworkRequestsConcurrently()
+            val forwardedTime = testScheduler.apply { advanceTimeBy(1000); runCurrent() }
+
+            // Assert
+            Assert.assertEquals(
+                listOf(
+                    UiState.Loading, UiState.Success(
+                        listOf(
+                            mockVersionFeaturesOreo,
+                            mockVersionFeaturesPie,
+                            mockVersionFeaturesAndroid10
+                        )
+                    )
+                ), receivedStates
+            )
+
+            Assert.assertEquals(
+                1000, forwardedTime.currentTime
+            )
+        }
+
+    private fun observeViewModel(viewModel: PerformNetworkRequestsConcurrentlyViewModel) {
+        viewModel.uiState().observeForever { uiState ->
+            if (uiState != null) {
+                receivedStates.add(uiState)
+            }
+        }
+    }
+}
